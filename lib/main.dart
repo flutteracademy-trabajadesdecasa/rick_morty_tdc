8f9f657a79c3ac768306baf3289ed195b90f1451
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2024-01-22 10:15:12 +0100

    INIT FIREBASE

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_morty_tdc/core/locator/locator.dart';
 import 'package:rick_morty_tdc/core/router/app_router.dart';
+import 'package:rick_morty_tdc/firebase_options.dart';
 import 'package:rick_morty_tdc/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(const MainApp());
 }
